51522991c4e51d8d0186b17876b077ad023f4ba0
greatt8696 <greatt8696@gmail.com>
2022-12-21 00:06:11 +0900

    web3Manager

--- backend/web3/web3Manager.js
+++ backend/web3/web3Manager.js
@@ -1,9 +1,8 @@
 const Web3 = require("web3");
 
-const FROM = "0x879F90de3f5E39567be8C090a33219d30d5EC33e"; // 첫번째 계정
-const CA = "0x9Bae0edfA21976f5b5476B4d295a0fE1C14D3E36";
-const Contract = require("../contracts/Test.json");
-const { Transaction, Block } = require("../models");
+const CA = "0x0C851690471aCfc7466264C93865169B781624aF";
+const Contract = require("../../solidity/artifacts/Test.json");
+// const { Transaction, Block } = require("../models");
 // setInterval(async () => {
 //   web3.eth.getAccounts(console.log);
 //   const blockNum = await web3.eth.getBlockNumber();
@@ -11,77 +10,81 @@ const { Transaction, Block } = require("../models");
 //   web3.eth.getTransaction(CA).then(console.log);
 // }, 1000);
 
-const web3 = new Web3(Web3.givenProvider || "ws://127.0.0.1:8545");
-
-async function blocksFromWeb3(web3) {
-  const blockNum = await web3.eth.getBlockNumber(); // 최대갯수 : 600
-  const blocks = [];
-  return new Promise((resolve, reject) => {
-    Array(blockNum)
-      .fill(false)
-      .forEach(async (_, idx) => {
-        const getBlock = await web3.eth.getBlock(idx, true);
-        blocks.push(getBlock);
-        if (idx === blockNum - 1) resolve(blocks);
-      });
-  });
-}
+// async function blocksFromWeb3(web3) {
+//   const blockNum = await web3.eth.getBlockNumber(); // 최대갯수 : 600
+//   const blocks = [];
+//   return new Promise((resolve, reject) => {
+//     Array(blockNum)
+//       .fill(false)
+//       .forEach(async (_, idx) => {
+//         const getBlock = await web3.eth.getBlock(idx, true);
+//         blocks.push(getBlock);
+//         if (idx === blockNum - 1) resolve(blocks);
+//       });
+//   });
+// }
 
 class TransactionManager {
-  constructor(web3) {
-    this.web3 = web3;
+  constructor() {
+    this.web3 = new Web3(Web3.givenProvider || "ws://127.0.0.1:8545");
   }
 
   init = async function () {
     this.instance = await new this.web3.eth.Contract(Contract.abi, CA);
     this.subscribeAllEvent();
+    this.subscribeTransationEvent(this.instance);
+    console.log(await this.getAccounts());
+  };
+
+  subscribeTransationEvent = function (contractInstance) {
+    contractInstance.events
+      .allEvents(function (error, result) {
+        if (!error) {
+          console.log("subscribeTransationEvent : ", result);
+          return;
+        }
+        console.error(error);
+      })
+      .on("connected", function (subscriptionId) {
+        console.log(subscriptionId);
+      })
+      .on("error", console.error);
   };
 
   subscribeAllEvent = function () {
-    const newBlockHeaders = new web3.eth.subscribe("newBlockHeaders", function (
-      error,
-      result
-    ) {
-      if (!error) {
-        console.log("newBlockHeaders : ", result);
-        return;
-      }
-      console.error(error);
-    })
+    this.web3.eth
+      .subscribe("newBlockHeaders", function (error, result) {
+        if (!error) {
+          console.log("newBlockHeaders : ", result.hash, result.number);
+          return;
+        }
+        console.error(error);
+      })
       .on("connected", function (subscriptionId) {
         console.log(subscriptionId);
       })
       .on("data", async function (blockHeader) {
-        blockHeader["transactions"] = [];
-        console.log("@@newBlockHeaders=>transaction@@ : ", blockHeader);
-        const getLatestBlock = await web3.eth.getBlock("latest", true);
-        await Block.insertBlock(getLatestBlock);
-        getLatestBlock.transactions.forEach(async (transaction) => {
-          console.log("Transaction:newTransaction", transaction);
-          await Transaction.insertTransaction(transaction);
-        });
+        // console.log("@@newBlockHeaders=>transaction@@ : ", blockHeader);
       })
       .on("error", console.error);
 
-    const latestBlocks = this.instance.events.allEvents(
-      { fromBlock: "latest" },
-      async (err, result) => {
-        console.log("@@@BEGIN@@ : ", result);
-        const newTransaction = await new web3.eth.getTransaction(result.hash);
+    // const latestBlocks = this.instance.events.allEvents(
+    //   { fromBlock: "latest" },
+    //   async (err, result) => {
+    //     console.log("@@@BEGIN@@ : ", result);
+    //     const newTransaction = await this.web3.eth.getTransaction(result.hash);
 
-        await Transaction.insertTransaction(newTransaction);
-        // console.log("@@@BEGIN@@ : ", JSON.stringify(result));
-      }
-    );
+    //     // await Transaction.insertTransaction(newTransaction);
+    //     // console.log("@@@BEGIN@@ : ", JSON.stringify(result));
+    //   }
+    // );
   };
+
+  getAccounts = async () => await this.web3.eth.getAccounts();
 }
 
-const getAllBlocks = async () => blocksFromWeb3(web3);
-
-const getTransactionManager = async () => new TransactionManager(web3);
-
 const autoContractTanscation = async (loopSize = 1000, duration = 10000) => {
-  const instance = await new web3.eth.Contract(Contract.abi, CA);
+  const instance = await this.web3.eth.Contract(Contract.abi, CA);
   let i = 0;
   const intervalId = setInterval(async () => {
     if (i >= loopSize) clearInterval(intervalId);
@@ -94,9 +97,6 @@ const autoContractTanscation = async (loopSize = 1000, duration = 10000) => {
   }, duration);
 };
 
-module.exports = {
-  web3,
-  getAllBlocks,
-  getTransactionManager,
-  autoContractTanscation,
-};
+const transactionManager = new TransactionManager();
+
+transactionManager.init();
