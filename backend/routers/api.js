d12842ed927b8ed77e95f0ea1d8f1bf28f355d77
greatt8696 <88331836+greatt8696@users.noreply.github.com>
2022-12-22 11:21:05 +0900

    backend set api, frontend init store, reducer

--- backend/routers/api.js
+++ backend/routers/api.js
@@ -10,4 +10,14 @@ const router = require("express").Router();
 //   return res.sendFile(process.cwd() + `\\images\\${id}.jpg`);
 // });
 
+router.get("/block/:page", async (req, res) => {
+  const { page } = req.params;
+  return res.send(page);
+});
+
+// router.get("/transaction/:id", async (req, res) => {
+//   const { id } = req.params;
+//   return res.sendFile(process.cwd() + `\\images\\${id}.jpg`);
+// });
+
 module.exports = router;
