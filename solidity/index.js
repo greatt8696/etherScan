e467ddd97ab400a39d1b253c1d52cbc58f0f2171
greatt8696 <greatt8696@gmail.com>
2022-12-26 01:50:32 +0900

    123

--- solidity/index.js
+++ solidity/index.js
@@ -1,7 +1,8 @@
-const CA = [
-  "0x43F2F33775f591E4D60393e7E74264d65Fb0A0F6",
-  "0xd9145CCE52D386f254917e481eB44e9943F39138",
-];
+const CA = ["0x43F2F33775f591E4D60393e7E74264d65Fb0A0F6"];
+//const CA = [
+//  "0x43F2F33775f591E4D60393e7E74264d65Fb0A0F6",
+//  "0xd9145CCE52D386f254917e481eB44e9943F39138",
+//];
 const Contract = require("./artifacts/TestTransition.json");
 
 module.exports = { CA, Contract };
